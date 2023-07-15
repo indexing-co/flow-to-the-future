380278fddaea2eef3729b039cf8caa2680e48230
Francisco Antunes <francisco@vilavelosa.com>
2023-07-15 21:35:15 +0100

    Update layout of nfts page when there isn't too many items

--- frontend/app/nfts/[wallet]/page.tsx
+++ frontend/app/nfts/[wallet]/page.tsx
@@ -20,42 +20,41 @@ export default async function NFTs({
   }[];
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 min-h-screen">
       <Header title="Select up to 3 nfts" wallet="0xa62d...3178" />
       <p className="text-lg text-white text-center -mt-14 mb-10">
         Don’t worry, the Flow capacitor is non-custodial and will not burn your
         existing NFTs.
       </p>
-      <form
-        action={sendNFTs}
-        className="flex flex-wrap gap-4 justify-center mb-10"
-      >
-        {data
-          .filter(
-            (d) =>
-              d &&
-              d.name &&
-              (d.image_thumbnail_url || d.image_url) &&
-              d.token_id
-          )
-          .map((item) => (
-            <label htmlFor={`nft-${item.token_id}`} key={item.token_id}>
-              <input
-                type="checkbox"
-                name={`nft-${item.token_id}`}
-                id={`nft-${item.token_id}`}
-                value={item.token_id}
-                className="peer hidden"
-              />
-              <Image
-                src={item.image_thumbnail_url || item.image_url}
-                alt={item.name}
-                height={300}
-                width={300}
-                className="cursor-pointer border-8 border-white peer-checked:border-green-1 peer-focus:border-blue-400"
-              />
-            </label>
-          ))}
+      <form action={sendNFTs} className="flex flex-col gap-8 mb-10">
+        <div className="flex flex-wrap w-full gap-4 justify-center">
+          {data
+            .filter(
+              (d) =>
+                d &&
+                d.name &&
+                (d.image_thumbnail_url || d.image_url) &&
+                d.token_id
+            )
+            .map((item) => (
+              <label htmlFor={`nft-${item.token_id}`} key={item.token_id}>
+                <input
+                  type="checkbox"
+                  name={`nft-${item.token_id}`}
+                  id={`nft-${item.token_id}`}
+                  value={item.token_id}
+                  className="peer hidden"
+                />
+                <Image
+                  src={item.image_thumbnail_url || item.image_url}
+                  alt={item.name}
+                  height={300}
+                  width={300}
+                  className="cursor-pointer border-8 border-white peer-checked:border-green-1 peer-focus:border-blue-400"
+                />
+              </label>
+            ))}
+        </div>
         <button
           type="submit"
           className="flex items-center gap-4 py-4 px-32 m-auto rounded border bg-black uppercase font-bold text-white transition-all hover:bg-white hover:text-black"
