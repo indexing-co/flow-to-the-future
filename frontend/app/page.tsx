70e25e9e2a1ccf9fb7ea7ff93b174be237c92db1
Francisco Antunes <francisco@vilavelosa.com>
2023-07-17 00:48:18 +0100

    Remove unused global state management

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@ import checkWallet from "../actions/check-wallet";
 import Header from "@/components/header";
 import Arrow from "@/icons/arrow";
 import SubmitArrowIcon from "@/icons/submit-arrow";
-import { handleChangeWallet } from "@/utils";
 
 export default function Home() {
   return (
@@ -22,7 +21,6 @@ export default function Home() {
               required
               // autoComplete="off"
               name="wallet"
-              onChange={handleChangeWallet}
               className="bg-black/30 w-[95%] mb-10 rounded px-6 py-6 text-xl outline outline-2 outline-offset-2 outline-green-1 text-white"
             />
           </div>
